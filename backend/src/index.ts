c7518a77eb5c76bbe67f30c56e8d41543babeb1a
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-03-14 12:12:19 +0800

    setup graphql with apllo server experss

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,29 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constanst";
-import mikroConfig from './mikro-orm.config'
+import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolvers } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
-  await orm.getMigrator().up()
+  await orm.getMigrator().up();
+
+  const app = express();
+
+  const aplloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolvers],
+      validate: false,
+    }),
+  });
+
+  aplloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("App start on port 4000");
+  });
 };
 
 main();
---
+++ backend/src/resolvers/hello.ts
@@ -0,0 +1,11 @@
+import { Query, Resolver } from "type-graphql";
+
+@Resolver()
+export class HelloResolvers {
+
+    @Query(()=> String)
+    hello() {
+        return 'Hello world'
+    }
+
+}
