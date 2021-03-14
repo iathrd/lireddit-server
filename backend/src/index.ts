e96c5dae855538622ab705fc0cdcdbe48e6405d5
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-03-14 11:35:58 +0800

    setup Mikro ORM

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello world')
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constanst";
+import mikroConfig from './mikro-orm.config'
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up()
+};
+
+main();
