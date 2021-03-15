73b39173ee97b512cd642431fdba93c4f5929daa
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-03-15 14:37:04 +0800

    created register mutation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import 'reflect-metadata'
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constanst";
 import mikroConfig from "./mikro-orm.config";
@@ -7,6 +7,7 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { HelloResolvers } from "./resolvers/hello";
 import { PostResolvers } from "./resolvers/post";
+import { UserResolvers } from "./resolvers/user";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
@@ -16,7 +17,7 @@ const main = async () => {
 
   const aplloServer = new ApolloServer({
     schema: await buildSchema({
-      resolvers: [HelloResolvers, PostResolvers],
+      resolvers: [HelloResolvers, PostResolvers, UserResolvers],
       validate: false,
     }),
     context: () => ({ em: orm.em }),
