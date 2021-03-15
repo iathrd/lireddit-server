dfa1c16a39121039cef9a2fb0d1477d1507c60f2
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-03-15 12:43:55 +0800

    created User entities

--- backend/src/mikro-orm.config.ts
+++ backend/src/mikro-orm.config.ts
@@ -1,16 +1,17 @@
 import { __prod__ } from "./constanst";
 import { Post } from "./entities/Post";
-import {MikroORM} from '@mikro-orm/core'
-import path from 'path'
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+import { User } from "./entities/User";
 
 export default {
-    migrations:{
-    path: path.join(__dirname,'./migrations'),// path to the folder with migrations
+  migrations: {
+    path: path.join(__dirname, "./migrations"), // path to the folder with migrations
     pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
-    },
-    entities:[Post],
-    dbName: "lireddit",
-    password: "Iqbal081322",
-    type:'postgresql',
-    debug: !__prod__
-  } as Parameters<typeof MikroORM.init>[0];
+  },
+  entities: [Post, User],
+  dbName: "lireddit",
+  password: "Iqbal081322",
+  type: "postgresql",
+  debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
