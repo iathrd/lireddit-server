e75f5772abe4663d57a790973db35b72b51dc60d
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-03-16 15:03:45 +0800

    craeted navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import { Box, Flex, Link } from "@chakra-ui/layout";
+import React from "react";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
+import { Button } from "@chakra-ui/button";
+
+interface NavbarProps {}
+
+export const Navbar: React.FC<NavbarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  if (fetching) {
+    body = null;
+  } else if (!data?.me) {
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    body = (
+      <Flex>
+        <Box mr={4}>{data.me.username}</Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    );
+  }
+
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>{body}</Box>
+    </Flex>
+  );
+};
