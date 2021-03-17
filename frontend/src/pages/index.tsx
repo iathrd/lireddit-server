bc1f086abaf5f2f6b4822562b6b1184d78c85dfd
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-03-17 19:53:59 +0800

    added withUrqlclient on login and register

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,20 +4,19 @@ import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 
 const Index = () => {
-  const [{ data }] = usePostsQuery();
+
+  const [{data}] = usePostsQuery()
 
   return (
     <>
       <Navbar />
       <div>Hello world</div>
-      <br />
-      {!data ? (
-        <div>loading...</div>
-      ) : (
-        data.posts.map((data) => <div key={data.id}>{data.title}</div>)
-      )}
+      <br/>
+      {!data? <div>loading...</div> : data.posts.map(data=>(
+        <div key={data.id} >{data.title}</div>
+      ))}
     </>
   );
 };
 
-export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
+export default withUrqlClient(createUrqlClient,{ssr:true})(Index);
