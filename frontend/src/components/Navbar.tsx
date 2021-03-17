01126d4b7b375907626c44c1d84eb24fa4c0a1cf
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-03-17 19:46:35 +0800

    changed grapql to SSR

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import { Box, Flex, Link } from "@chakra-ui/layout";
 import React from "react";
 import NextLink from "next/link";
-import { useMeQuery } from "../generated/graphql";
+import { useMeQuery, useLogoutMutation } from "../generated/graphql";
 import { Button } from "@chakra-ui/button";
 
 interface NavbarProps {}
 
 export const Navbar: React.FC<NavbarProps> = ({}) => {
+  const [{fetching:logoutFetching}, logout] = useLogoutMutation();
   const [{ data, fetching }] = useMeQuery();
   let body = null;
 
@@ -27,7 +28,9 @@ export const Navbar: React.FC<NavbarProps> = ({}) => {
     body = (
       <Flex>
         <Box mr={4}>{data.me.username}</Box>
-        <Button variant="link">Logout</Button>
+        <Button onClick={() => logout()} isLoading={logoutFetching} variant="link">
+          Logout
+        </Button>
       </Flex>
     );
   }
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,23 @@
-import { Navbar } from "../components/Navbar"
+import { withUrqlClient } from "next-urql";
+import { Navbar } from "../components/Navbar";
+import { usePostsQuery } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 
-const Index = () => (
-  <>
-  <Navbar/>
-  <div>Hello world</div>
-  </>
-)
+const Index = () => {
+  const [{ data }] = usePostsQuery();
 
-export default Index
+  return (
+    <>
+      <Navbar />
+      <div>Hello world</div>
+      <br />
+      {!data ? (
+        <div>loading...</div>
+      ) : (
+        data.posts.map((data) => <div key={data.id}>{data.title}</div>)
+      )}
+    </>
+  );
+};
+
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
