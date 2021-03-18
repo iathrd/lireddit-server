e1d33a3dfff28aeaabffbf5fc88e19f04b736aa7
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-03-18 10:01:08 +0800

    handle mutation on navbar if user logout

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -3,12 +3,15 @@ import React from "react";
 import NextLink from "next/link";
 import { useMeQuery, useLogoutMutation } from "../generated/graphql";
 import { Button } from "@chakra-ui/button";
+import { isServer } from "../utils/isServer";
 
 interface NavbarProps {}
 
 export const Navbar: React.FC<NavbarProps> = ({}) => {
   const [{fetching:logoutFetching}, logout] = useLogoutMutation();
-  const [{ data, fetching }] = useMeQuery();
+  const [{ data, fetching }] = useMeQuery({
+    pause:isServer()
+  });
   let body = null;
 
   if (fetching) {
