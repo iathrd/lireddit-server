1c562504f2c8a12e89a3b7fccb54abaf81f7665c
Iqbal Athorid <iqbalathoridd@gmail.com>
2021-04-04 09:54:50 +0800

    added email on User entities

--- backend/src/resolvers/user.ts
+++ backend/src/resolvers/user.ts
@@ -39,6 +39,12 @@ class UserResponse {
 
 @Resolver()
 export class UserResolvers {
+  @Mutation(() => Boolean)
+  async forgotPassword(@Arg("email") email: string, @Ctx() { em }: MyContext) {
+    const user = await em.findOne(User, { email });
+    return true
+  }
+
   @Query(() => User, { nullable: true })
   async me(@Ctx() { req, em }: MyContext): Promise<User | null> {
     if (!req.session.userId) {
